8dc70f2c1107a1f79ba1c2d5f2ad631b7b1948ec
Patrick Miles <patrick.miles@gmail.com>
2018-03-19 21:31:36 -0700

    testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,10 +15,10 @@
 
   $("#add-train").on("click", function(event) {
 
-    $("#name-input").val().trim();
-    $("#destination-input").val().trim();
-    $("#time-input").val().trim();
-    $("#frequency-input").val().trim();
+   var name = $("#name-input").val().trim();
+   var destination =  $("#destination-input").val().trim();
+   var time = $("#time-input").val().trim();
+   var frequency = $("#frequency-input").val().trim();
 
   var newTrain = {
 
@@ -42,6 +42,41 @@
 
 });
 
+database.ref().on("child_added", function(snapshot, prevChildKey) {
 
 
+var newName = snapshot.val().name;
+var newDest = snapshot.val().destination;
+var time = snapshot.val().time;
+var frequency = snapshot.val().frequency;
 
+console.log(newName);
+console.log(newDest);
+console.log(time);
+console.log(frequency);
+
+var tFrequency = frequency;
+var firstTime = newTime;
+
+var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+console.log(firstTimeConverted);
+
+var currentTime = moment();
+console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+console.log("DIFFERENCE IN TIME: " + diffTime);
+
+tRemainder = diffTime % tFrequency;
+console.log(tRemainder);
+
+var tMinutesTillTrain = tFrequency - tRemainder;
+console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))
+
+$("#trains > tbody").append("<tr><td>" + newName + "</td><td>" + newDest + "</td><td>" +
+frequency + "</td><td>" + moment(nextTrain).format("hh:mm") + "</td><td>" + tMinutesTillTrain + "</td><td>");
+
+});
