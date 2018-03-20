b2ce3c8d6f79ef5a39f76cee34c979293e275555
Patrick Miles <patrick.miles@gmail.com>
2018-03-19 17:23:27 -0700

    moment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,42 +36,19 @@
 
   };
 
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
+
+
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+
+
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
     console.log(childSnapshot.val());
 
-    var tName = childSnapshot.val().name;
- var tDestination = childSnapshot.val().destination;
- var tFrequency = childSnapshot.val().frequency;
- var tFirstTrain = childSnapshot.val().firstTrain;
-
- var timeArr = tFirstTrain.split(":");
- var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
- var maxMoment = moment.max(moment(), trainTime);
- var tMinutes;
- var tArrival;
- 
- //If the first train is later than the current time, sent arrival to the first train time
- if (maxMoment === trainTime) {
-   tArrival = trainTime.format("hh:mm A");
-   tMinutes = trainTime.diff(moment(), "minutes");
- } else {
-
-   // Calculate the minutes until arrival using hardcore math
-   // To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time
-   // and find the modulus between the difference and the frequency.
-   var differenceTimes = moment().diff(trainTime, "minutes");
-   var tRemainder = differenceTimes % tFrequency;
-   tMinutes = tFrequency - tRemainder;
-   // To calculate the arrival time, add the tMinutes to the currrent time
-   tArrival = moment().add(tMinutes, "m").format("hh:mm A");
- }
-   console.log("tMinutes:", tMinutes);
-   console.log("tArrival:", tArrival);
-
-   // Add each train's data into the table
-   $("#train-table > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" +
-     tFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
- });
 
 
+  });
